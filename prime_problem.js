293a28df4dfb6d7b1b7493801baadc9dcab2b128
aman <amangauti3@gmail.com>
2022-09-22 23:10:19 +0530

    aman01

---
+++ prime_problem.js
@@ -0,0 +1,14 @@
+
+let count = 0;
+for (i = 13; i <= num; i++) {
+for ( let i = 0; i <= num; i++) {
+    if (num % i == 0) {
+        count++;
+}if (count<=2){
+
+        console.log("Ys");
+    } else {
+   } else {
+        console.log("No");
+
+}
