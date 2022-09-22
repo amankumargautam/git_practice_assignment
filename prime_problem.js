423453ba6381bf40a3395c11d4e639029d18be25
rajneesh <mr.rajneeshy@gmail.com>
2022-09-22 23:49:37 +0530

    m1 code fixed

--- prime_problem.js
+++ prime_problem.js
@@ -1,13 +1,16 @@
-let num = 20;
+
 let count = 0;
+let num=13
 for ( let i = 0; i <= num; i++) {
     if (num % i == 0) {
         count++;
 }if (count<=2){
 
         console.log("Yes");
-     
-   } else {
+    } else {
+  
         console.log("No");
+
+}
 
 }
