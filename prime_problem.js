12bbd3cebd223561b85771d15c9a80fd97e38d57
aman <amangauti3@gmail.com>
2022-09-22 23:29:59 +0530

    M2_code_fix

--- prime_problem.js
+++ prime_problem.js
@@ -1,13 +1,12 @@
-
+let num = 20;
 let count = 0;
-for (i = 13; i <= num; i++) {
 for ( let i = 0; i <= num; i++) {
     if (num % i == 0) {
         count++;
 }if (count<=2){
 
-        console.log("Ys");
-    } else {
+        console.log("Yes");
+     
    } else {
         console.log("No");
 
